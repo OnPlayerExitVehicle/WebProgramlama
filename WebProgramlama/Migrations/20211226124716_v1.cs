2529f85cb3d7a5df018ad87cc7c6ff2e304d6ef4
Ali Nazif Koca <onplayerexitvehicle@outlook.com>
2021-12-26 22:37:45 +0300

    add migrations

---
+++ WebProgramlama/Migrations/20211226124716_v1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProgramlama.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ad",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Soyad",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Soyad",
+                table: "AspNetUsers");
+        }
+    }
+}
